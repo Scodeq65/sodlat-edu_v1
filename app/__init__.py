e276bf31f392056d48e3a86835468cbed0987449
scodeq65 <sodiqagbaraojo@gmail.com>
2024-08-30 23:09:17 +0100

    done

--- app/__init__.py
+++ app/__init__.py
@@ -14,10 +14,14 @@ def create_app():
     db.init_app(app)
     migrate.init_app(app, db)
     login.init_app(app)
-    login.login_view = 'auth.login'  # Redirect unauthorized users to the login page
+    login.login_view = 'auth.login'
 
     from app.routes import main, auth
     app.register_blueprint(main.bp)
     app.register_blueprint(auth.bp)
 
     return app
+
+@login.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
