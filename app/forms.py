2d95fd822c5086e6e08e7c73710f63fd7e9fba8b
scodeq65 <sodiqagbaraojo@gmail.com>
2024-08-31 03:11:18 +0100

    no_quiting

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, SelectField, TextAreaField
+from wtforms import StringField, PasswordField, SubmitField, SelectField, TextAreaField, DateTimeField, IntegerField
 from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
-from app.models import User
+from app.models import User, Course, Assignment
+from datetime import datetime
+from wtforms.ext.sqlalchemy.fields import QuerySelectField
 
 class LoginForm(FlaskForm):
     """Login form."""
@@ -16,7 +18,7 @@ class RegistrationForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[
         DataRequired(), EqualTo('password')])
-    role = SelectField('Role', choices=[('parent', 'Parent'), ('teacher', 'Teacher'), ('student', 'Student')])
+    role = SelectField('Role', choices=[('parent', 'Parent'), ('teacher', 'Teacher'), ('student', 'Student')], validators=[DataRequired()])
     submit = SubmitField('Register')
 
     def validate_username(self, username):
@@ -33,7 +35,7 @@ class UserForm(FlaskForm):
     """Form for creating or updating users."""
     username = StringField('Username', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
-    role = SelectField('Role', choices=[('parent', 'Parent'), ('teacher', 'Teacher'), ('student', 'Student')])
+    role = SelectField('Role', choices=[('parent', 'Parent'), ('teacher', 'Teacher'), ('student', 'Student')], validators=[DataRequired()])
     password = PasswordField('Password')
     confirm_password = PasswordField('Confirm Password', validators=[
         EqualTo('password')])
@@ -49,14 +51,36 @@ class UserForm(FlaskForm):
         if user:
             raise ValidationError('Email is already registered.')
 
+class CourseForm(FlaskForm):
+    """Form for creating or updating courses."""
+    name = StringField('Course Name', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+class AssignmentForm(FlaskForm):
+    """Form for creating or updating assignments."""
+    title = StringField('Title', validators=[DataRequired()])
+    content = TextAreaField('Content', validators=[DataRequired()])
+    due_date = DateTimeField('Due Date', format='%Y-%m-%d %H:%M:%S', validators=[DataRequired()])
+    course_id = QuerySelectField('Course', query_factory=lambda: Course.query.all(), get_label='name', allow_blank=False, validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+    def validate_due_date(self, due_date):
+        if due_date.data < datetime.utcnow():
+            raise ValidationError('Due date cannot be in the past.')
+
 class LinkParentForm(FlaskForm):
     """Form for linking a student to a parent."""
     parent_name = StringField('Parent Username', validators=[DataRequired()])
     parent_email = StringField('Parent Email', validators=[DataRequired(), Email()])
     submit = SubmitField('Link Parent')
 
-    def validate_parent(self, parent_name, parent_email):
-        parent = User.query.filter_by(username=parent_name.data, email=parent_email.data).first()
+    def validate_parent_name(self, parent_name):
+        parent = User.query.filter_by(username=parent_name.data).first()
+        if not parent:
+            raise ValidationError('No matching parent found.')
+
+    def validate_parent_email(self, parent_email):
+        parent = User.query.filter_by(email=parent_email.data).first()
         if not parent:
             raise ValidationError('No matching parent found.')
 
@@ -64,12 +88,12 @@ class ProgressForm(FlaskForm):
     """Form for tracking student progress."""
     student_name = StringField('Student Name', validators=[DataRequired()])
     teacher_name = StringField('Teacher Name', validators=[DataRequired()])
-    grade = StringField('Grade', validators=[DataRequired()])
-    attendance = StringField('Attendance')
+    grade = IntegerField('Grade', validators=[DataRequired()])
+    attendance = IntegerField('Attendance')
     overall_performance = TextAreaField('Overall Performance')
     submit = SubmitField('Submit')
 
-    def validate_student(self, student_name):
+    def validate_student_name(self, student_name):
         student = User.query.filter_by(username=student_name.data).first()
         if not student:
             raise ValidationError('No matching student found.')
